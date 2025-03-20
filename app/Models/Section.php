34f05490e8d0d60463319f549ef043ede963822c
jacksih <jackysihombing33@gmail.com>
2025-03-20 15:18:00 +0700

    Inisiasi Belajar Filament: Resource

---
+++ app/Models/Section.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Section extends Model
+{
+    use HasFactory;
+    protected $table = 'sections';
+    protected $fillable = ['class_id', 'name'];
+
+    public function classes()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+}
